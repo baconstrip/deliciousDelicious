46441ec55e0feb4a297cb298c61ba86450709e86
Jeffrey Spragg <jwspragg@mtu.edu>
2016-02-13 04:55:59 -0500

    Finished module loader

--- ModuleLoader.py
+++ ModuleLoader.py
@@ -4,19 +4,32 @@ import json
 import re
 import os
 
-f = []
-for (dirpath, dirnames, filenames) in walk(os.getcwd() + "/modules/"):
-    f.extend(filenames)
-    break
+def loadModules():
+    f = []
+    for (dirpath, dirnames, filenames) in walk(os.getcwd() + "/modules/"):
+        f.extend(filenames)
+        break
 
-pat = re.compile('.*?\.json')
+    pat = re.compile('.*?\.json')
 
-jsonfiles = []
+    jsonfiles = []
 
-for candidate in f:
-    if(pat.match(candidate)):
-        jsonfiles.append(candidate)
+    for candidate in f:
+        if(pat.match(candidate)):
+            jsonfiles.append(candidate)
 
-for jsonfile in jsonfiles:
-    definition = json.load(open(os.getcwd() + "/modules/" + jsonfile))
-    pprint(definition)
+    modules = []
+
+    for jsonfile in jsonfiles:
+        definition = json.load(open(os.getcwd() + "/modules/" + jsonfile))
+        pyfile = definition.get("pyfile")
+        classname = definition.get("classname")
+        # Load and execute the pyfile specified
+        exec(open(os.getcwd() + "/modules/" + pyfile).read())
+        # Exec the module's constructor
+        constructor = globals()[classname]
+        # instantiate
+        instance = constructor()
+        modules.append(instance)
+
+    return modules
