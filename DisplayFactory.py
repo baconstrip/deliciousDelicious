747e163fcb088c7461dcb247331508b65709968d
Jason Haisma <Jason Haisma>
2016-02-13 17:05:07 -0500

    Set up the Display Factory

---
+++ DisplayFactory.py
@@ -0,0 +1,16 @@
+import json
+
+class DisplayFactor:
+    
+    def __init__()
+
+    with open('config.json', 'r') as json_data:
+    config = json.load(json_data)
+
+
+    # Exec the module's constructor
+    constructor = globals()[config['display']]
+    # instantiate
+    instance = constructor(config['data'])
+    modules.append(instance)
+
