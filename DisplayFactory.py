3c98baebcf36adbf638c791f2b68d7b107bfcec0
Jason Haisma <Jason Haisma>
2016-02-13 17:38:54 -0500

    Fixed obvious issues with factory

--- DisplayFactory.py
+++ DisplayFactory.py
@@ -2,15 +2,15 @@ import json
 
 class DisplayFactor:
     
-    def __init__()
+    def __init__():
 
-    with open('config.json', 'r') as json_data:
-    config = json.load(json_data)
+        with open('config.json', 'r') as json_data:
+            config = json.load(json_data)
 
 
-    # Exec the module's constructor
-    constructor = globals()[config['display']]
-    # instantiate
-    instance = constructor(config['data'])
-    modules.append(instance)
+        # Exec the module's constructor
+        constructor = globals()[config['display']]
+        # instantiate
+        instance = constructor(config['data'])
+        modules.append(instance)
 
