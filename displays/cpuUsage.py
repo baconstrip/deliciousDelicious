99748891b8546e36f1215c312539b7207c71ba13
Jason Haisma <Jason Haisma>
2016-02-13 11:25:01 -0500

    Fixed the way the script ran so it actually worked.

--- displays/cpuUsage.py
+++ displays/cpuUsage.py
@@ -1,27 +1,26 @@
 import paramiko
+from Display import Display
+from layout.Container import Container
+from layout.Label import Label
+from layout.PercentBar import PercentBar
+from layout.Value import Value
 
 class cpuUsage(Display):
+    name = "cpuUsage"
 
-	ssh = paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(
-	paramiko.AutoAddPolicy())
+    def __init__(self):
+        Display.__init__(self)
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        filename=open("./Login.txt")
+        user=filename.read().split()
 
-	filename = open("./Login.txt")
-	user=filename.read().split()
+        ssh.connect('wopr.csl.mtu.edu',username=user[0],password=user[1])
 
-	ssh.connect('wopr.csl.mtu.edu',username=user[0],password=user[1])
-
-	stdin, stdout, stderr = ssh.exec_command("top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\([0-9.]*\)%* id.*/\1/\" | awk '{print 100 - $1\"%\"}'")
-	stdoutData = stdout.read()
-
-	container1 = Container("container1", 0)
-	layout.append(container1)
-
-	label1 = Label("label1", 1)
-	label1.text="CPU Usage:"
-	container1.addElement(label1)
-
-	value1 = Label("value1", 2)
-	value1.value=stdoutData
-	container1.addElement(value1)
-
+        stdin, stdout, stderr = ssh.exec_command("top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\([0-9.]*\)%* id.*/\1/\" | awk '{print 100 - $1\"%\"}'")
+        stdoutData = stdout.read()
+        self.addElement(Container("Container", 0))
+        self.addElement(Label("Label", 1, "CPU Usage"))
+        self.addElement(PercentBar("PercentageBar", 2, 10))
+        self.addElement(Value("Value", 3, stdoutData))
+        
