d62e7bb6b440238896242b9fd6f10f94ec85e5b9
Jason Haisma <Jason Haisma>
2016-02-13 23:32:45 -0500

    Fixed the issue with Linux Server

--- displays/linuxServer.py
+++ displays/linuxServer.py
@@ -14,22 +14,25 @@ class LinuxServer(Display):
         filename=open("./Login.txt")
         user=filename.read().split()
 
-        reachable=ssh.connect('wopr.csl.mtu.edu',username=user[0],password=user[1], timeout=1000)
+        ssh.connect('wopr.csl.mtu.edu',username=user[0],password=user[1], timeout=1000)
 
         # Check if we reached the server
-        if reachable:
-            self.addElement(Label("Label0", 4, "Able to connect successfully!", "green"))
-            stdin1, stdout1, stderr1 = ssh.exec_command("top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\([0-9.]*\)%* id.*/\1/\"")
-            print(stdout1.read())
-            stdoutData1 = stdout1.read()
-            stdin2, stdout2, stderr2 = ssh.exec_command("grep MemTotal /proc/meminfo | awk '{print $2}")
-            stdoutData2 = stdout2.read()
 
-            self.addElement(Container("Container", 0))
-            self.addElement(Label("Label1", 1, "CPU Usage"))
-            self.addElement(PercentBar("PercentageBar", 2, stdoutData1))
-            self.addElement(Label("Label2", 3, stdoutData2))
+        stdin1, stdout1, stderr1 = ssh.exec_command("grep Active /proc/meminfo | awk \'{print $2}\'")
+        stdoutData1 = stdout1.readline()
+        stdin2, stdout2, stderr2 = ssh.exec_command("grep MemTotal /proc/meminfo | awk \'{print $2}\'")
+        stdoutData2 = stdout2.readline()
 
-        else:
-            self.addElement(Label("Label2", 4, "Not able to connect to the server!", "red"))
-        
+        stdin3, stdout3, stderr3 = ssh.exec_command("top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\([0-9.]*\)%* id.*/\1/\" | awk \'{print 100 - $1\"%\"}'")
+        stdoutData3 = stdout3.readline()
+
+
+        self.addElement(Container("Container", 0))
+        self.addElement(Label("Label1", 1, "Memory Used:"))
+        self.addElement(Label("Label2", 2, stdoutData1))
+        self.addElement(Label("Label3", 3, "Maximum Mememory:"))
+        self.addElement(Label("Label4", 4, stdoutData2))
+
+        self.addElement(Label("Label5", 5, "CPU Usage:"))
+        self.addElement(Label("Label6", 6, stdoutData3))
+
